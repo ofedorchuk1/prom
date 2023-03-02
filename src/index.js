e3ad1e6aa1505730e399ffbafb7e96567f736af6
Olha Khomych <o.fedorchuk1@gmail.com>
2023-03-02 20:33:20 +0200

    update hp

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-      <HashRouter>
+      <BrowserRouter>
           <App />
-      </HashRouter>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
